9488e294ea157bbb242019ebf6944fe604c5e4c8
Orlando Brea <orlando.brea@gmail.com>
2019-10-05 20:24:45 -0300

    Agrego axios

---
+++ 04-listado-axios/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Listado from './Listado/Listado';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Listado />
+    </div>
+  );
+}
+
+export default App;
